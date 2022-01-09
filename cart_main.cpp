11e218ffd9f0c64903c21a1dcdb01828eef82192
Gunjan-vyas <73429092+Gunjan-vyas@users.noreply.github.com>
2022-01-09 09:43:28 +0530

    Add files via upload

---
+++ cart_main.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include"datamodel.h"
+#include <vector>
+using namespace std;
+
+vector<Product> allProducts={
+	Product(1,"apple",26),
+	Product(3,"mango",16),
+	Product(2,"guava",36),
+	Product(5,"banana",56),
+	Product(4,"strawberry",29),
+	Product(6,"pineapple",20),
+};
+Product* chooseProduct(){
+	//Display list of products
+	string productList;
+	cout<<"Available products"<<endl;
+	for(auto product: allProducts){
+		productList.append(product.getDisplayName());
+	}
+	cout<<productList<<endl;
+	cout<<"~~~~~~~~~~~~~~~"<<endl;
+    string choice;
+	cin>>choice;
+	//linear search
+	for(int i=0;i<allProducts.size();i++){
+		if(allProducts[i].getShortName()==choice){
+			return &allProducts[i];
+		}
+	}
+	cout<<"product not found : "<<endl;
+	return NULL;
+}
+bool checkout(Cart &cart){
+	if(cart.isEmpty()){
+		return false;
+	}
+	int total = cart.getTotal();
+	cout<<"Pay in cash";
+	int paid;
+	cin>>paid;
+	if(paid>=total){
+		cout<<"Change "<<paid-total<<endl;
+		cout<<"Thank you for shopping!";
+		return true;
+	}
+	else{
+		cout<<"Not enough cash!";
+		return false;
+	}
+}
+int main(){
+		char action ;
+		Cart cart;
+		while(true){
+			cout<<"Select an action -  (a)dd item, (v)iew cart, (c)heckout"<<endl;
+			cin>>action;
+			if(action == 'a'){
+				// add to cart and
+				// view all products also choose product+add to cart;
+				Product*product=chooseProduct();
+				if(product!=NULL){
+					cout<<" Added to the cart "<<product->getDisplayName()<<endl;
+					cart.addProduct(*product);
+				}
+			}
+			else if(action=='v'){
+				cout<<"-----------------"<<endl;
+				cout<<cart.viewCart();
+				cout<<"-----------------"<<endl;
+				
+			}
+			else{
+				 //checkout
+				 cart.viewCart();
+				 if(checkout(cart)){
+				 	break;
+				 }
+			}
+		}
+		
+}
